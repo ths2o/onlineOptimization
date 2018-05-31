064861aa8d13df1fe5451ae147cf7e7e63bb0a8b
서태희 <Taehee@seotaehuiui-MacBook-Pro-2.local>
2018-05-31 21:42:07 +0900

    avazu stream data

---
+++ src/main/scala/AvazuStream.scala
@@ -0,0 +1,33 @@
+/**
+  * Created by Taehee on 2018. 5. 31..
+  */
+
+object AvazuStream {
+
+
+
+  import java.io.{FileOutputStream, FileInputStream}
+  import java.io.PrintStream
+  import java.util.zip.GZIPInputStream
+
+  import scala.io.Source
+
+
+
+  def main(args: Array[String]): Unit = {
+
+
+    val reader = new GZIPInputStream(new FileInputStream("~/stream/avazu/train.gz"))
+    val writer = new PrintStream(new FileOutputStream("~/stream/avazu/avazu-train", true))
+    //val filename = "/Users/Taehee/Downloads/test"
+    for (line <- Source.fromInputStream(reader).getLines) {
+
+      println(line)
+      writer.append(line + "\n")
+      Thread.sleep(10)
+
+    }
+    writer.close()
+
+  }
+}
